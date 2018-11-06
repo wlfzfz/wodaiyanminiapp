9683cd5428f4325ca8b219d9c9fd9d895ae63330
wlfzfz <13659693538@163.com>
2018-11-06 20:16:37 +0800

    第一次提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DemoApplication {
 
+    //启动springboot
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
